84141c511ef50e6727ba8c697e04d395e55152bc
karlley <karlley.jp@gmail.com>
2024-06-05 08:03:52 +0900

    prettierで整形

--- 01.fizzbuzz/fizzbuzz.js
+++ 01.fizzbuzz/fizzbuzz.js
@@ -1,13 +1,13 @@
 #!/usr/bin/env node
 
-const numbers = [...Array(20).keys()].map(i => i + 1);
+const numbers = [...Array(20).keys()].map((i) => i + 1);
 numbers.forEach((number) => {
     if (number % 15 === 0) {
-        console.log('FizzBuzz');
+        console.log("FizzBuzz");
     } else if (number % 5 === 0) {
-        console.log('Buzz');
+        console.log("Buzz");
     } else if (number % 3 === 0) {
-        console.log('Fizz');
+        console.log("Fizz");
     } else {
         console.log(number.toString());
     }
