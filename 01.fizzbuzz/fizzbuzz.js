4b200d8de678c3bb7a8ae0d02e7cdf3c86075aa4
karlley <karlley.jp@gmail.com>
2024-06-06 09:08:15 +0900

    ルールに沿ったインデントに修正、条件式の変更

--- 01.fizzbuzz/fizzbuzz.js
+++ 01.fizzbuzz/fizzbuzz.js
@@ -2,13 +2,13 @@
 
 const numbers = [...Array(20).keys()].map((i) => i + 1);
 numbers.forEach((number) => {
-    if (number % 15 === 0) {
-        console.log("FizzBuzz");
-    } else if (number % 5 === 0) {
-        console.log("Buzz");
-    } else if (number % 3 === 0) {
-        console.log("Fizz");
-    } else {
-        console.log(number.toString());
-    }
+  if (number % 3 === 0 && number % 5 === 0) {
+    console.log("FizzBuzz");
+  } else if (number % 5 === 0) {
+    console.log("Buzz");
+  } else if (number % 3 === 0) {
+    console.log("Fizz");
+  } else {
+    console.log(number.toString());
+  }
 });
