ad29cce0c6aab4fde462bb274bb8527bc2424cb7
karlley <karlley.jp@gmail.com>
2024-09-12 08:03:05 +0900

    Promiseでラップした関数を使用した実装に変更

--- 03.asynchronous/promise.js
+++ 03.asynchronous/promise.js
@@ -4,66 +4,132 @@ import sqlite3 from "sqlite3";
 const db = new sqlite3.Database(":memory:");
 const titles = ["書籍1", "書籍2", "書籍3"];
 
+const runPromise = (sql, params = [], callback = null) => {
+  return new Promise((resolve, reject) => {
+    db.run(sql, params, function (error) {
+      if (error) {
+        if (callback) {
+          callback(error);
+        } else {
+          reject(error);
+        }
+      } else {
+        if (callback) {
+          callback(null);
+        } else {
+          resolve(this);
+        }
+      }
+    });
+  });
+};
+
+const allPromise = (sql, params = [], callback = null) => {
+  return new Promise((resolve, reject) => {
+    db.all(sql, params, function (error, rows) {
+      if (error) {
+        if (callback) {
+          callback(error);
+        } else {
+          reject(error);
+        }
+      } else {
+        if (callback) {
+          callback(null, rows);
+        } else {
+          resolve(rows);
+        }
+      }
+    });
+  });
+};
+
+const closePromise = (callback = null) => {
+  return new Promise((resolve, reject) => {
+    db.close(function (error) {
+      if (error) {
+        if (callback) {
+          callback(error);
+        } else {
+          reject(error);
+        }
+      } else {
+        if (callback) {
+          callback(null);
+        } else {
+          resolve(null);
+        }
+      }
+    });
+  });
+};
+
+const createTableSQL = `CREATE TABLE Books
+                        (
+                            id    INTEGER PRIMARY KEY AUTOINCREMENT,
+                            title TEXT UNIQUE
+                        )`;
+
+const createBooksSQL = `INSERT INTO Books (title)
+                        VALUES (?)`;
+
+const getBooksSQL = `SELECT *
+                     FROM Books`;
+
+const deleteTableSQL = `DROP TABLE Books`;
+
 const createTable = () => {
-  db.run(
-    `CREATE TABLE Books
-         (
-             id    INTEGER PRIMARY KEY AUTOINCREMENT,
-             title TEXT UNIQUE
-         )`,
-    () => {
-      createBooks(titles);
-    },
-  );
+  return runPromise(createTableSQL);
 };
 
 const createBooks = (titles, index = 0) => {
   if (titles.length === index) {
-    getBooks();
-    return;
+    return Promise.resolve();
   }
 
-  db.run(
-    `INSERT INTO Books (title)
-         VALUES (?)`,
-    [titles[index]],
-    function () {
-      console.log(`ID: ${this.lastID} created.`);
-      createBooks(titles, index + 1);
-    },
-  );
+  return runPromise(createBooksSQL, [titles[index]]).then((result) => {
+    console.log(`ID: ${result.lastID} created.`);
+    return createBooks(titles, index + 1);
+  });
 };
 
 const getBooks = () => {
-  db.all(
-    `SELECT *
-         FROM Books`,
-    (error, books) => {
-      displayBooks(books);
-    },
-  );
+  return allPromise(getBooksSQL);
 };
 
 const displayBooks = (books) => {
   books.forEach((book) => {
     console.log(`ID: ${book.id}, Title: ${book.title}`);
   });
-  deleteTable();
+  return Promise.resolve();
 };
 
 const deleteTable = () => {
-  db.run(`DROP TABLE Books`, () => {
-    closeDB();
-  });
+  return runPromise(deleteTableSQL);
 };
 
 const closeDB = () => {
-  db.close();
-  return;
+  return closePromise();
 };
 
 function main() {
-  createTable();
+  createTable()
+    .then(() => {
+      return createBooks(titles);
+    })
+    .then(() => {
+      return getBooks();
+    })
+    .then((books) => {
+      return displayBooks(books);
+    })
+    .then(() => {
+      return deleteTable();
+    })
+    .then(() => {
+      return closeDB();
+    })
+    .then(() => {});
 }
 
 main();
