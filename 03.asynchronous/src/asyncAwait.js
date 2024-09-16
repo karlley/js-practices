9c037f58a9d62b89aed775bd4171ed2f040635d2
karlley <karlley.jp@gmail.com>
2024-09-17 07:31:45 +0900

    async/await用のファイル追加

---
+++ 03.asynchronous/src/asyncAwait.js
@@ -0,0 +1,69 @@
+#!/usr/bin/env node
+import {
+  runPromise,
+  allPromise,
+  closePromise,
+} from "../db/promiseFunctions.js";
+import {
+  createTableSQL,
+  createBooksSQL,
+  getBooksSQL,
+  deleteTableSQL,
+} from "../db/queries.js";
+
+const titles = ["書籍1", "書籍2", "書籍3"];
+
+const createTable = () => {
+  return runPromise(createTableSQL);
+};
+
+const createBooks = (titles, index = 0) => {
+  if (titles.length === index) {
+    return Promise.resolve();
+  }
+
+  return runPromise(createBooksSQL, [titles[index]]).then((result) => {
+    console.log(`ID: ${result.lastID} created.`);
+    return createBooks(titles, index + 1);
+  });
+};
+
+const getBooks = () => {
+  return allPromise(getBooksSQL);
+};
+
+const displayBooks = (books) => {
+  books.forEach((book) => {
+    console.log(`ID: ${book.id}, Title: ${book.title}`);
+  });
+  return Promise.resolve();
+};
+
+const deleteTable = () => {
+  return runPromise(deleteTableSQL);
+};
+
+const closeDB = () => {
+  return closePromise();
+};
+
+function main() {
+  createTable()
+    .then(() => {
+      return createBooks(titles);
+    })
+    .then(() => {
+      return getBooks();
+    })
+    .then((books) => {
+      return displayBooks(books);
+    })
+    .then(() => {
+      return deleteTable();
+    })
+    .finally(() => {
+      closeDB();
+    });
+}
+
+main();
