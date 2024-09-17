5486aebc4d57e24efdc2b8ab5736567d945a500e
karlley <karlley.jp@gmail.com>
2024-09-18 07:27:29 +0900

    Promiseでラップした関数の不要な処理を削除

--- 03.asynchronous/db/promiseFunctions.js
+++ 03.asynchronous/db/promiseFunctions.js
@@ -2,61 +2,37 @@ import sqlite3 from "sqlite3";
 
 const db = new sqlite3.Database(":memory:");
 
-const runPromise = (sql, params = [], callback = null) => {
+const runPromise = (sql, params = []) => {
   return new Promise((resolve, reject) => {
     db.run(sql, params, function (error) {
       if (error) {
-        if (callback) {
-          callback(error);
-        } else {
-          reject(error);
-        }
+        reject(error);
       } else {
-        if (callback) {
-          callback(null);
-        } else {
-          resolve(this);
-        }
+        resolve(this);
       }
     });
   });
 };
 
-const allPromise = (sql, params = [], callback = null) => {
+const allPromise = (sql, params = []) => {
   return new Promise((resolve, reject) => {
     db.all(sql, params, function (error, rows) {
       if (error) {
-        if (callback) {
-          callback(error);
-        } else {
-          reject(error);
-        }
+        reject(error);
       } else {
-        if (callback) {
-          callback(null, rows);
-        } else {
-          resolve(rows);
-        }
+        resolve(rows);
       }
     });
   });
 };
 
-const closePromise = (callback = null) => {
+const closePromise = () => {
   return new Promise((resolve, reject) => {
     db.close(function (error) {
       if (error) {
-        if (callback) {
-          callback(error);
-        } else {
-          reject(error);
-        }
+        reject(error);
       } else {
-        if (callback) {
-          callback(null);
-        } else {
-          resolve(null);
-        }
+        resolve(null);
       }
     });
   });
