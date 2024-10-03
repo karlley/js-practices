a81f1d9c2c4646cfeffcfb8c9e1c16f56af576eb
karlley <karlley.jp@gmail.com>
2024-10-04 07:54:08 +0900

    SQL内の余分な空白を削除

--- 03.asynchronous/db/queries.js
+++ 03.asynchronous/db/queries.js
@@ -1,17 +1,10 @@
-const createTableSQL = `CREATE TABLE Books
-                        (
-                            id    INTEGER PRIMARY KEY AUTOINCREMENT,
-                            title TEXT UNIQUE
-                        )`;
-const insertBookSQL = `INSERT INTO Books (title)
-                       VALUES (?)`;
-const invalidInsertBookSQL = `INSERT INTO InvalidTable (title)
-                              VALUES (?)`;
-const selectBookSQL = `SELECT *
-                       FROM Books`;
-const invalidSelectBookSQL = `SELECT *
-                              FROM InvalidTable`;
-const dropTableSQL = `DROP TABLE Books`;
+const createTableSQL =
+  "CREATE TABLE Books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT UNIQUE)";
+const insertBookSQL = "INSERT INTO Books (title) VALUES (?)";
+const invalidInsertBookSQL = "INSERT INTO InvalidTable (title) VALUES (?)";
+const selectBookSQL = "SELECT * FROM Books";
+const invalidSelectBookSQL = "SELECT * FROM InvalidTable";
+const dropTableSQL = "DROP TABLE Books";
 
 export {
   createTableSQL,
